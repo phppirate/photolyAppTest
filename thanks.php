a14f83ec16bfcfcc31dddd408e1c01dbb6d745db
phppirate <sam@podlogar.com>
2013-12-11 13:24:20 -0600

    added thank u page

--- thanks.php
+++ thanks.php
@@ -14,26 +14,13 @@
     <div class="panel panel-primary">
       <div class="panel-heading"><h4 class="text-center">Upload a photo to Photo.ly</h4></div>
       <div class="panel-body">
-        <form action="thanks.php">
-          <div>
-            <label for="photo">Photos</label>
-            <input type="file" id="photo">
-          </div>
-          <br/>
-          <div>
-            <label for="caption">Add Caption</label>
-            <input type="text" id="caption" placeholder="Your Caption" class="form-control">
-          </div>
-          <br/>
-          <div>
-            <label for="comment">Add Comment</label>
-            <textarea id="comment" class="form-control" rows="10px"></textarea>
-          </div>
-          <br/>
-          <div>
-            <input type="submit" class="btn btn-success btn-block btn-lg" value="UPLOAD">
-          </div>
-        </form>
+        <h5>Your Photo was Uploaded</h5>
+        <p>
+          Thank you for using Photo.ly
+        </p>
+        <p>
+          <a href="index.php" class="btn btn-block btn-primary btn-lg">« Back</a>
+        </p>
       </div>
       <div class="panel-footer">
         <h4 class="text-center">&copy; 2013 - PHOTO.LY</h4>
